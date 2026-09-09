63a9c3137a6132da5dc1e77fb526a24c27bc43f1
Elias Jaktlund <jaktlundelias@gmail.com>
2026-09-09 10:58:50 +0200

    lagt till nästlade for-loopar

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
+using System.Data;
+using System.Security.AccessControl;
 
-Console.WriteLine("Ange storlek på brädet genom att skriva ett tal mellan 3-50");
-String input = Console.ReadLine();
 
-int.TryParse(input, out int size);
+	Console.WriteLine("Ange storlek på brädet genom att skriva ett tal mellan 3-50");
+	String input = Console.ReadLine();
 
-Console.WriteLine("Storleken på brädet är: " + size);
+	int.TryParse(input, out int storlek);
 
-if (size >= 3 && size <= 50)
+	Console.WriteLine("Storleken på brädet är: " + storlek);
+
+if (storlek >= 3 && storlek <= 50)
 {
-	Console.WriteLine("Godkänd storlek!");
+    Console.WriteLine("Godkänd storlek!");
+    for (int rad = 0; rad < storlek; rad++)
+    {
+        for (int kolumn = 0; kolumn < storlek; kolumn++)
+        {
+            if ((rad + kolumn) % 2 == 0)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            
+        }
+        Console.WriteLine();
+    }
 }
 else
 {
-	Console.WriteLine("Felaktig storlek, mata in ett tal mellan 3-50");
+    Console.WriteLine("Felaktig storlek, mata in ett tal mellan 3-50");
 }
-
