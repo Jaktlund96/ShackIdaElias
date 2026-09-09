5287d24d1072fda86657879369bdcf6043c36a85
idafunseth-tech <ida.funseth@gmail.com>
2026-09-09 10:11:59 +0200

    Första frasen, vad användaren svarar och vilka storlekar som är godkända

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Ange storlek på brädet genom att skriva ett tal mellan 3-50");
+String input = Console.ReadLine();
+
+int.TryParse(input, out int size);
+
+Console.WriteLine("Storleken på brädet är: " + size);
+
+if (size >= 3 && size <= 50)
+{
+	Console.WriteLine("Godkänd storlek!");
+}
+else
+{
+	Console.WriteLine("Felaktig storlek, mata in ett tal mellan 3-50");
+}
+
