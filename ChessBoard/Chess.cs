9190de8d645f414a8f13a379c32c4865c589000f
Elias Jaktlund <jaktlundelias@gmail.com>
2026-09-16 10:29:57 +0200

    la till missade semikolon och namespace

--- ChessBoard/Chess.cs
+++ ChessBoard/Chess.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
-class Chess
+namespace ChessBoard
 {
-    public static int ReadSize() //Metod 1. Läser in storleken och skriver ut storleken
+    class Chess
     {
-  while (true)
-    {
-			Console.WriteLine("Ange storlek på brädet genom att skriva ett tal mellan 3-50");
-			String input = Console.ReadLine();
-            if (int.TryParse(input, out int size) && size >=3 && size <=50)
+        public static int ReadSize() //Metod 1. Läser in storleken och skriver ut storleken
+        {
+            while (true)
             {
-                Console.WriteLine("Godkänt storlek!");
-                return size;
-            }
+                Console.WriteLine("Ange storlek på brädet genom att skriva ett tal mellan 3-50");
+                String input = Console.ReadLine();
+                if (int.TryParse(input, out int size) && size >= 3 && size <= 50)
+                {
+                    Console.WriteLine("Godkänt storlek!");
+                    return size;
+                }
 
-            Console.WriteLine("Felaktig storlek, skriv ett tal mellan 3-50.)
-		}
+                Console.WriteLine("Felaktig storlek, skriv ett tal mellan 3-50.");
+            }
+        }
     }
 }
