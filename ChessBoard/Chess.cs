caa4148a1bcdfb1f8c2ef99dad8fa8d594d9e4b9
idafunseth-tech <ida.funseth@gmail.com>
2026-09-16 10:19:50 +0200

    Lagt till en metod i klassen

--- ChessBoard/Chess.cs
+++ ChessBoard/Chess.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ChessBoard
+class Chess
 {
-    internal class Chess
+    public static int ReadSize() //Metod 1. Läser in storleken och skriver ut storleken
     {
+  while (true)
+    {
+			Console.WriteLine("Ange storlek på brädet genom att skriva ett tal mellan 3-50");
+			String input = Console.ReadLine();
+            if (int.TryParse(input, out int size) && size >=3 && size <=50)
+            {
+                Console.WriteLine("Godkänt storlek!");
+                return size;
+            }
+
+            Console.WriteLine("Felaktig storlek, skriv ett tal mellan 3-50.)
+		}
     }
 }
